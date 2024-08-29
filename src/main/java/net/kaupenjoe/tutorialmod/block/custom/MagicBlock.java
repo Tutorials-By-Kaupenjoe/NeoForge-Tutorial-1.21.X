1b1f0e0a7d68b632c7e649908af40fc6dd347bdb
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-08-29 10:52:55 +0200

    custom tags

--- src/main/java/net/kaupenjoe/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/net/kaupenjoe/tutorialmod/block/custom/MagicBlock.java
@@ -1,6 +1,7 @@
 package net.kaupenjoe.tutorialmod.block.custom;
 
 import net.kaupenjoe.tutorialmod.item.ModItems;
+import net.kaupenjoe.tutorialmod.util.ModTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
@@ -35,7 +36,7 @@ public class MagicBlock extends Block {
     @Override
     public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
         if(entity instanceof ItemEntity itemEntity) {
-            if(itemEntity.getItem().getItem() == ModItems.RAW_BISMUTH.get()) {
+            if(isValidItem(itemEntity.getItem())) {
                 itemEntity.setItem(new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
             }
 
@@ -45,6 +46,10 @@ public class MagicBlock extends Block {
         }
 
         super.stepOn(level, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack item) {
+        return item.is(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
