259bdd896d6ac45f087fe6227c8a299a33aa82fa
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-09-06 14:18:00 +0200

    datamaps

--- src/main/java/net/kaupenjoe/tutorialmod/datagen/DataGenerators.java
+++ src/main/java/net/kaupenjoe/tutorialmod/datagen/DataGenerators.java
@@ -33,6 +33,9 @@ public class DataGenerators {
         generator.addProvider(event.includeServer(), blockTagsProvider);
         generator.addProvider(event.includeServer(), new ModItemTagProvider(packOutput, lookupProvider, blockTagsProvider.contentsGetter(), existingFileHelper));
 
+        generator.addProvider(event.includeServer(), new ModDataMapProvider(packOutput, lookupProvider));
+
+
         generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
     }
---
+++ src/main/java/net/kaupenjoe/tutorialmod/datagen/ModDataMapProvider.java
@@ -0,0 +1,23 @@
+package net.kaupenjoe.tutorialmod.datagen;
+
+import net.kaupenjoe.tutorialmod.item.ModItems;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.DataMapProvider;
+import net.neoforged.neoforge.registries.datamaps.builtin.FurnaceFuel;
+import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModDataMapProvider extends DataMapProvider {
+    protected ModDataMapProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookupProvider) {
+        super(packOutput, lookupProvider);
+    }
+
+    @Override
+    protected void gather() {
+        this.builder(NeoForgeDataMaps.FURNACE_FUELS)
+                .add(ModItems.STARLIGHT_ASHES.getId(), new FurnaceFuel(1200), false)
+                .add(ModItems.FROSTFIRE_ICE.getId(), new FurnaceFuel(2400), false);
+    }
+}
