08eef541d835643d85122e2a60d8203bedbbc5be
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-11-24 20:08:05 +0100

    Worldgen Setup

---
+++ src/main/java/net/kaupenjoe/tutorialmod/worldgen/ModBiomeModifiers.java
@@ -0,0 +1,23 @@
+package net.kaupenjoe.tutorialmod.worldgen;
+
+import net.kaupenjoe.tutorialmod.TutorialMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+public class ModBiomeModifiers {
+
+    public static void bootstrap(BootstrapContext<BiomeModifier> context) {
+        // CF -> PF -> BM
+        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+    }
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, name));
+    }
+}
