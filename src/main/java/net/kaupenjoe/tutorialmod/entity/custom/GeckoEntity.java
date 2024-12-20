30c14527e64c0623858d6924220dd6a810a63442
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-12-20 11:47:43 +0100

    custom boss bar

--- src/main/java/net/kaupenjoe/tutorialmod/entity/custom/GeckoEntity.java
+++ src/main/java/net/kaupenjoe/tutorialmod/entity/custom/GeckoEntity.java
@@ -5,12 +5,16 @@ import net.kaupenjoe.tutorialmod.entity.ModEntities;
 import net.kaupenjoe.tutorialmod.item.ModItems;
 import net.minecraft.Util;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.BossEvent;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
@@ -30,6 +34,9 @@ public class GeckoEntity extends Animal {
 
     private static final EntityDataAccessor<Integer> VARIANT =
             SynchedEntityData.defineId(GeckoEntity.class, EntityDataSerializers.INT);
+
+    private final ServerBossEvent bossEvent =
+            new ServerBossEvent(Component.literal("Our Mighty Gecko"), BossEvent.BossBarColor.GREEN, BossEvent.BossBarOverlay.NOTCHED_10);
 
     public GeckoEntity(EntityType<? extends Animal> entityType, Level level) {
         super(entityType, level);
@@ -146,5 +153,24 @@ public class GeckoEntity extends Animal {
     @Override
     protected SoundEvent getDeathSound() {
         return SoundEvents.BAT_DEATH;
+    }
+
+    /* BOSS BAR */
+    @Override
+    public void startSeenByPlayer(ServerPlayer serverPlayer) {
+        super.startSeenByPlayer(serverPlayer);
+        this.bossEvent.addPlayer(serverPlayer);
+    }
+
+    @Override
+    public void stopSeenByPlayer(ServerPlayer serverPlayer) {
+        super.stopSeenByPlayer(serverPlayer);
+        this.bossEvent.removePlayer(serverPlayer);
+    }
+
+    @Override
+    public void aiStep() {
+        super.aiStep();
+        this.bossEvent.setProgress(this.getHealth() / this.getMaxHealth());
     }
 }
